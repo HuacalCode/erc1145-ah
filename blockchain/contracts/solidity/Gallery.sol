4bc3a3016731de9f984fe0c58eb0ecb686243f9f
aescuderoc <aescuderoc@EKT1778949MACLAP.local>
2022-05-09 17:03:37 -0500

    Adds struct for Non fungibles with stats

--- blockchain/contracts/solidity/Gallery.sol
+++ blockchain/contracts/solidity/Gallery.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "../../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 
 contract Gallery is ERC1155 {
 
@@ -10,7 +11,16 @@ contract Gallery is ERC1155 {
     uint256 public constant ARMOR = 2;
     uint256 public constant ACCESORY = 3;
 
+    struct Weapon {
+        
+    }
+
     constructor(address faucetAddress) public ERC1155("https://game.example/api/item/{id}.json") {
         _mint(faucetAddress, GOLD, 100**18, "");
+    }
+
+    function itsDangerousOutside(address player1, address player2, address player3) public {
+        _mint(player1, WEAPON, 1, data);
+        _MI
     }
 }
